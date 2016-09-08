8a059030203961bfab84e2e86c7ebc25237f644a
acadev CEO <nasro.edu@gmail.com>
2016-09-08 18:50:09 -0400

    created a test class to test Cohesion

---
+++ Lecture2/src/TestCohesion.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author nasro
+ *
+ */
+public class TestCohesion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
